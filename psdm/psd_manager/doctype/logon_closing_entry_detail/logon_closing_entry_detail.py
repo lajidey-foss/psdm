fd4dfa2d4ba4b09a396437b4945f88da22c37291
lajidey-foss <spryng.managed@gmail.com>
2026-03-29 17:01:20 +0000

    App Doctypes Creation
    Create, configuration of doctypes and child doctypes, also configure all needed moving parts like auto fetch of data value and other options

---
+++ psdm/psd_manager/doctype/logon_closing_entry_detail/logon_closing_entry_detail.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Jide Olayinka [Pivotage Integrated] and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LogonClosingEntryDetail(Document):
+	pass
