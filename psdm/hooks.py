83c31dd72f76e729167481113a97b4ab16f23a4a
lajidey-foss <spryng.managed@gmail.com>
2026-04-30 00:32:16 +0000

    Auto generate pay wallet from payment entry
    Now begin the pipeline from payment optional is now possible, on submit of payment pay wallet is created with wip data

--- psdm/hooks.py
+++ psdm/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"Payment Entry": {
+        "on_update": "psdm.utils.app.wallet_pay_update",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
@@ -256,7 +254,8 @@ fixtures = [
                 (
                     "Purchase Invoice-custom_logon",
                     "Sales Invoice-custom_logon",
-                    "Sales Invoice-custom_allocation"
+                    "Sales Invoice-custom_allocation",
+                    "Payment Entry-custom_wallet_bank_ref"
                 )
             ]
         ]
